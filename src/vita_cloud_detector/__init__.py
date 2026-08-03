a991819bf4462238b12dd0191e5dfaa35b69f39a
Gab1604 <84352210+Gab1604@users.noreply.github.com>
2026-08-03 21:23:34 +0300

    Export Sentinel-2 GeoTIFF adapter

--- src/vita_cloud_detector/__init__.py
+++ src/vita_cloud_detector/__init__.py
@@ -3,6 +3,7 @@ from .config import DetectorConfig
 from .core import CLASS_NAMES, class_statistics, run_array, strict_valid_mask
 from .models import DetectionResult, OutputPaths
 from .sentinel2 import run_sentinel2
+from .sentinel2_geotiff import run_sentinel2_geotiff
 
 __all__ = [
     "CLASS_NAMES",
@@ -13,6 +14,7 @@ __all__ = [
     "run_array",
     "run_balkan1",
     "run_sentinel2",
+    "run_sentinel2_geotiff",
     "strict_valid_mask",
 ]
 
